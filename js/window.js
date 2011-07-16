371965f77e32606d8fc41519bca5545731a5f8ce
Soumia ADJIR <adjir.soumia@gmail.com>
2011-07-16 20:53:00 +0200

    modified

--- js/window.js
+++ js/window.js
@@ -7,9 +7,10 @@
             .toggle();
         });
 
-        $("#taskbar .level1.menugroup .itemlevel1.item1 :regex(class,^itemlevel2 item\\d+$)").children("a").click(function(e){
-            e.preventDefault();
-            $button_menu = $(this);
+        $("#taskbar .level1.menugroup .itemlevel1.item1 :regex(class,^itemlevel2 item\\d+$)").children("a").click(function(event){
+            event.preventDefault();
+
+            var $button_menu = $(this);
             $button_menu
                 .parents("#container")
                 .find("#window div.body")
@@ -23,7 +24,7 @@
                 type : "GET",
                 url : page,
                 success:function(data){
-                    $("#body_window").text(data);
+                    $("#window div.body").load(data);
                 }
             });
         });
