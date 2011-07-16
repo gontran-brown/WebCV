93a8f2d27c56c0244ce6c34051429f234a28603e
Jérémy CARLIER <jeremy.carlier@pleinnet.fr>
2011-07-16 19:14:32 +0200

    Je t'es fait une petite partie j'te laisse deviner le reste mais j'reste là pour t'aider

--- js/window.js
+++ js/window.js
@@ -7,9 +7,13 @@
             .toggle();
         });
 
-        $("#taskbar .level1.menugroup .itemlevel1.item1 :regex(class,^itemlevel2 item\\d+$)").click(function(e){
+        $("#taskbar .level1.menugroup .itemlevel1.item1 :regex(class,^itemlevel2 item\\d+$)").children("a").click(function(e){
+            e.preventDefault();
             $button_menu = $(this);
-            $("div.body").empty();
+            $button_menu
+                .parents("#container")
+                .find("#window div.body")
+                .empty();
             var txt = $(this).attr("title");
             console.log(txt);
             var page = txt+".php";
