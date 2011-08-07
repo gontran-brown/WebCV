756514fa91f0ad196eb6a6706fc7e7d56b8d9e1e
Jérémy CARLIER <jeremy.carlier@pleinnet.fr>
2011-08-07 21:19:07 +0200

    Premier essai de fonctionnement

--- js/window.js
+++ js/window.js
@@ -14,31 +14,35 @@
 
         $("#taskbar .level1.menugroup .itemlevel1.item1 :regex(class,^itemlevel2 item\\d+$)").children("a").click(function(e){
             e.preventDefault();
-            $("#window").show();
-            var $button_menu = $(this);
-            var $body_window = $button_menu
+            var title_page = $(this).text();
+            var url_page = $(this).attr("href");
+
+            var $body_window = $(this)
             .parents("#container")
             .find("#window div.body");
-
             $body_window.empty();
-            var page_url = $button_menu.attr("href");
 
-            var $title_window = $button_menu
+            var $title_window = $(this)
             .parents("#container")
             .find("#window div.label > span:first-child");
-            title_page = $(this).text();
             $title_window.empty();
+
             $title_window.append(title_page);
 
             $.ajax({
                 type : "GET",
-                url : page_url,
+                url : url_page,
                 cache : false,
                 success : function(data){
-                    var new_content = $("#window div.body",data);
-                    $body_window.replaceWith(new_content);
+                    var $new_content = $("#window div.body",data);
+                    $body_window.append($new_content.children());
+                },
+                complete: function(){
+                    $body_window.children(".jScrollbar").jScrollbar();
                 }
             });
+
+            $body_window.children(".jScrollbar").jScrollbar();
         });
     });
 })(jQuery);
